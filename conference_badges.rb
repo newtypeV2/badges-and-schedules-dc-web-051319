8baa66386c62bb61a9e5427d00ebe4ff7fd95208
Gian Libunao <gianlibunao@gmail.com>
2019-04-26 13:55:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arrayNames)
+  batchBadge=[]
+  arrayNames.each do |n| batchBadge.push("Hello, my name is #{n}.")
+end
+end
